a6e75c09c245db2df14874170205f6e6715a2a41
Akila Kavinda Herath <kavindaakila619@gmail.com>
2026-06-18 19:36:39 +0530

    Add PostgreSQL bronze loader

---
+++ loaders/load_to_postgres.py
@@ -0,0 +1,47 @@
+import os
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, text
+
+
+load_dotenv()
+
+DB_HOST = os.getenv("POSTGRES_HOST")
+DB_PORT = os.getenv("POSTGRES_PORT")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
+CSV_PATH = "data/riyasewana_all_listings.csv"
+TABLE_NAME = "riyasewana_listings_raw"
+SCHEMA_NAME = "bronze"
+
+
+def main():
+    connection_url = (
+        f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}"
+        f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    )
+
+    engine = create_engine(connection_url)
+
+    df = pd.read_csv(CSV_PATH)
+
+    df["loaded_at"] = pd.Timestamp.now()
+
+    with engine.begin() as connection:
+        connection.execute(text("CREATE SCHEMA IF NOT EXISTS bronze;"))
+
+        df.to_sql(
+            TABLE_NAME,
+            connection,
+            schema=SCHEMA_NAME,
+            if_exists="replace",
+            index=False
+        )
+
+    print(f"Loaded {len(df)} rows into bronze.{TABLE_NAME}")
+
+
+if __name__ == "__main__":
+    main()
